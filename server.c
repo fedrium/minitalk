ac03d1cfb7b58e842601e0a3eac3dbff6e9c27a8
Yu Xiang <99579760+fedrium@users.noreply.github.com>
2022-09-27 00:00:39 +0800

    Add files via upload

--- server.c
+++ server.c
@@ -13,9 +13,26 @@
 #include "minitalk.h"
 #include "libft/libft.h"
 
-void signal_handler()
+void signal_handler(int sig, siginfo_t *info, void *ucontext)
 {
-	
+	static	int	i;
+	int bits;
+	static	char word;
+
+	i = 0;
+	chars = 0;
+	if (sig = SIGUSR2)
+		bits = 1;
+	else
+		bits = 0;
+	c <<= 1;
+	c = c + bits;
+	if (i > 8)
+	{
+		write(1, &c, 1);
+		i = 0;
+		c = 0;
+	}
 }
 
 int main()
@@ -24,6 +41,7 @@ int main()
 	pid_t pid;
 
 	sa.sa_handler = &signal_handler;
+	sa.sa_flag = SIG_INFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	pid = getpid();
